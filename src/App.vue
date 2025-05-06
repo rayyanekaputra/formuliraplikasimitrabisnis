ce417edd1090e16b59f8b4a137027b41bcfa12af
rayyan eka putra <rayyan.ekaputra@gmail.com>
2025-05-06 09:01:30 +0800

    first init daftar customer

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import RegistrasiCustomerView from './views/RegistrasiCustomerView.vue';
+import './styles.sass'
+</script>
+
+<template>
+  <RegistrasiCustomerView />
+</template>
+
+
---
+++ src/views/RegistrasiCustomerView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="registrasi-grid">
+        <h1>Formulir Aplikasi Mitra Bisnis</h1>
+        <h1>Formulir Aplikasi Mitra Bisnis</h1>
+        <h1>Formulir Aplikasi Mitra Bisnis</h1>
+        <h1>Formulir Aplikasi Mitra Bisnis</h1>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="sass" scoped>
+
+.registrasi-grid
+    display: grid
+    grid-template-columns: repeat(2, 1fr)
+    gap: 20px
+    outline: 1px solid #ccc
+</style>
